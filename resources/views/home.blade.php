d5aa5dd3665035b174dce7b8b502b4ce2fd5fa9f
blandine balde <blandinebalde@gmail.com>
2021-09-09 01:29:46 +0000

    Add 2 pages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>home</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
@@ -18,6 +18,6 @@
     </body>
 
     <footer>
-        <p>&copy; Copyrigth 2021 &middot; <a href="">About us </a></p>
+        <p>&copy; Copyrigth 2021 &middot; <a href="/about-us">About us </a></p>
     </footer>
 </html>
