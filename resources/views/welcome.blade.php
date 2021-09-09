8f40f0ff9cbbaf50e519f1b61fbe532a3999a574
blandine balde <blandinebalde@gmail.com>
2021-09-09 01:10:54 +0000

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+
+    </head>
+    <body>
+        <h1>hello from quebec</h1>
+        <p> it's currently {{date('H:i A')}}</p>
+    </body>
+
+    <footer>
+        <p>&copy; Copyrigth 2021 &middot; <a href="">About us </a></p>
+    </footer>
+</html>
